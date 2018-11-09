40a0200abc8f01aa834698078a3b9f90850b7bc3
Vaidas <vaidas.vaiciunas@inbox.lt>
2018-11-09 19:34:54 +0200

    Updates container initiation scripts

--- run-over-usb.sh
+++ run-over-usb.sh
@@ -3,16 +3,21 @@
 xhost +local:docker
 
 USER_UID=$(id -u)
+CONTAINER_EXIST=`docker ps -a --format "{{.Image}}" | grep gnuradio`
 
-docker run -d \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v `pwd`/gnuradio:/home/gnuradio \
-    -e DISPLAY=unix$DISPLAY \
-    --device /dev/bus/usb \
-    --device /dev/snd \
-    --volume=/run/user/${USER_UID}/pulse:/run/user/1000/pulse \
-    --name gnuradio \
-    --user root \
-    gnuradio
+if [ $CONTAINER_EXIST ]; then
+    docker start gnuradio
+else
+    docker run --rm -d \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v `pwd`/gnuradio:/home/gnuradio \
+        -e DISPLAY=unix$DISPLAY \
+        --device /dev/bus/usb \
+        --device /dev/snd \
+        --volume=/run/user/${USER_UID}/pulse:/run/user/1000/pulse \
+        --name gnuradio \
+        --user root \
+        gnuradio
+fi
 
 docker exec --user root gnuradio iio_info -s
